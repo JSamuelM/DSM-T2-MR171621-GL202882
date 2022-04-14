f219cc553e0ad437cf244fb3d8ea59e5ac4cb623
Samuel Mena <jmena@tec101.com>
2022-04-13 20:46:01 -0600

    Creando proyecto

---
+++ app/src/main/java/sv/edu/udb/dsmt2mr171621gl202882/MainActivity.kt
@@ -0,0 +1,11 @@
+package sv.edu.udb.dsmt2mr171621gl202882
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
