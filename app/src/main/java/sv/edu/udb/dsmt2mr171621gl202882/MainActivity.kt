d7de3346544b4d5ac5f8385a79712984c15828b9
Samuel Mena <jmena@tec101.com>
2022-04-16 17:42:23 -0600

    :sparkles: Agregando Bottom Navigation

--- app/src/main/java/sv/edu/udb/dsmt2mr171621gl202882/MainActivity.kt
+++ app/src/main/java/sv/edu/udb/dsmt2mr171621gl202882/MainActivity.kt
@@ -3,11 +3,22 @@ package sv.edu.udb.dsmt2mr171621gl202882
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import sv.edu.udb.dsmt2mr171621gl202882.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navHomeFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHomeFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
