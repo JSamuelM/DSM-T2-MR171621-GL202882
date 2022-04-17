45921a4717b6e03ce7dd8f16279ac436c4485ee2
Samuel Mena <jmena@tec101.com>
2022-04-16 22:58:05 -0600

    Agregando cambios

--- app/src/main/java/sv/edu/udb/dsmt2mr171621gl202882/MainActivity.kt
+++ app/src/main/java/sv/edu/udb/dsmt2mr171621gl202882/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import sv.edu.udb.dsmt2mr171621gl202882.databinding.ActivityMainBinding
 
@@ -18,7 +20,13 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = binding.bottomNavigationView
         val navHomeFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.shoppingCartFragment, R.id.logFragment)
+        )
+
         val navController = navHomeFragment.navController
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
     }
 }
